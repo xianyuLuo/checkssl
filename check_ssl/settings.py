6f9fc290cd88ba74e608432f9b432524f25f454c
luojiahao <luojiahao@dragonest.com>
2019-01-30 11:43:54 +0800

    bug修复：当第2个域名证书更新超时时，第二个域名的信息会继承为第一个域名信息

--- check_ssl/settings.py
+++ check_ssl/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from django.contrib import admin
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,7 +130,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-admin.site.site_header = 'RMS资源管理系统后台'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
