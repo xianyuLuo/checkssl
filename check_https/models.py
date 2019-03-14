ef1eed26fccc160f4932c7e9195c156fa7eb9528
luojiahao <luojiahao@dragonest.com>
2019-03-14 11:20:48 +0800

    增加域名为不重复字段

--- check_https/models.py
+++ check_https/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # 域名模型
 class domain(models.Model):
-    domain_name = models.CharField('域名', max_length = 50, help_text = "业务域名")
+    domain_name = models.CharField('域名', unique = True, max_length = 50, help_text = "业务域名")
     domain_port = models.IntegerField('端口', default = 443, help_text = "端口")
     domain_overseas = models.BooleanField('是否有海外HTTPS', default = False, help_text = "是否有海外https")
     domain_comment = models.TextField('备注', max_length = 500, blank = True, help_text = "备注")
@@ -16,7 +16,7 @@ class domain(models.Model):
 
 # 域名https检查模型
 class domain_https(models.Model):
-    domain_https_name = models.CharField('域名', max_length = 50, help_text = "业务域名")
+    domain_https_name = models.CharField('域名', unique = True, max_length = 50, help_text = "业务域名")
     domain_https_ca = models.CharField('CA中心', max_length = 50, help_text = "域名CA")
     domain_https_starttime = models.CharField('申请时间', max_length = 50, help_text = "域名证书申请时间")
     domain_https_endtime = models.CharField('到期时间', max_length = 50, help_text = "域名证书到期时间")
